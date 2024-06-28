893fa0516d55a9aaf1ff301294b0548b699ebb7d
Felipe Herrera <83690907+casafchile@users.noreply.github.com>
2024-06-28 17:50:44 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const TypesOfFruit = () => {
+  return (
+    <div>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+const Fruits = () => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+        <TypesOfFruit />                             //llama a la const de arriba y la muestra
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        { /* Change code below this line */ }
+          <Fruits/>                               //llama a la const de arriba y la muestra pero esta es el render principal, aqui muestra todo
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
